1ddd3bb641b6a7181d27358bcd2f476573d9207a
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-12-09 22:40:31 -0300

    Create ExibidorJson.java

---
+++ ExibidorJson.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ExibidorJson {
+JFrame f;
+
+public ExibidorJson(String jsonConvert){
+	f = new JFrame();
+	JOptionPane.showMessageDialog(f, jsonConvert);
+}
+}
